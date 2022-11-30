b9975c36b6b9d3a46551fdb326e6776d91255a07
Timothy Flach <tmflach@cpp.edu>
2022-11-30 14:00:23 -0800

    Finished initial version of totalAllowable.

--- finalCalc.c
+++ finalCalc.c
@@ -13,7 +13,8 @@ void totalIncurred(Expenses *e){
     printf("Meals: \t %f \n", e->total_meals); //assuming I can use the meal accumulator from Farid's branch
     printf("Hotel: \t %f \n", e->hotelExpenses);
     printf("Car Rental: \t %f \n", e->carRentals);
-    //need to add user input for gas used on a personal car.
+    //gas is assumed to be perfectly covered.
+    printf("Allowed Gas: \t %f \n", e->milesDriven * 0.27);
     printf("Parking Fees: \t %f", e->parkingFees);
     printf("Taxi Fees: \t %f \n", e->taxiFees);
     printf("Registration Fees: \t %f \n", e->registrationFees);
@@ -29,7 +30,19 @@ void totalAllowable(Expenses *e){
     //      Allowable:      ($37.00)
     //Hotels:               $200.10
     //      Allowable;      ($180.00)
-                    
+    printf("Allowable Expenses:\n");
+    //airfare is assumed to be completely covered and allowed by default.
+    printf("Airfare: \t (%f) \n", e->airfare);
+    printf("Meals: \t (%f) \n", e->total_allowable_meals);
+    printf("Hotel Expense: \t (%f)", 90.0 * (e->wholeDays + 1)); //Assuming 
+    //Car rental is assumed to be covered.
+    printf("Car Rental: \t (%f) \n", e->carRentals);
+    //Assume gas was perfectly covered.
+    printf("Gas: \t (%f) \n", e->milesDriven * 0.27);
+    printf("Parking Fees: \t (%f) \n", e->totalDays * 6.0);
+    printf("Taxi Fees: \t (%f) \n", e->totalDays * 10.0);
+    //Registation Fees assumed to be covered.
+    printf("Registration Fees: \t (%f) \n", e->registrationFees);                
 }
 void totalBill(Expenses *e){
     //display excess or amount saved
