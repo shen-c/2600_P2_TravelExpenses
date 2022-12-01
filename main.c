654d903a968ff72491c957ba4909a14ec4f0cabe
shen-c <congshen@cpp.edu>
2022-12-01 00:27:20 -0800

    minor formatting changes, FINAL

--- main.c
+++ main.c
@@ -5,19 +5,9 @@
 
 int main()
 {   
-    void fillExpenses(Expenses *e);//gets user input for all expenses (except meals)
-    void printExpenses(Expenses *e);//print expenses for validation
-    void getMeals(Expenses *e); //get user input for meals based on arrival/departure time
-                                //also calculates total meal expenses and total allowed based on time
-    void printMeals(Expenses *e);//print meals array for testing
-    void totalIncurred(Expenses *e);//calc and prints all costs incurred **and all allowables
-    void totalAllowable(Expenses *e);//calc and prints all allowables (not used)
-    void totalBill(Expenses *e);//prints amount saved or reimbursement required
-    
     Expenses trip1; // the struct holding all expenses
     Expenses *tripPtr = &trip1; //a pointer to pass the struct by reference into functions
     
-
     printf("\n*********************************");
     printf("\n*** Travel Expense Calculator ***");
     printf("\n*********************************\n\n");
@@ -37,17 +27,19 @@ int main()
     //get user input for each relevant meal
     getMeals(tripPtr);
     printf("\n");
+    
     //print the meals as a grid (for testing)
     //printMeals(tripPtr);
 
     //****** TASK 3 ******
     //implement "finalCalc"
 
-    //Note: the following functions are just guidelines,
     //display total incurred by line item
     totalIncurred(tripPtr);
+
     //display total allowed by line item
     //totalAllowable(tripPtr);
+
     //display amount saved or reimbursed
     totalBill(tripPtr);
 
